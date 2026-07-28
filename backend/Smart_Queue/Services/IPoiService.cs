5c13ca8731cd802953140a53e92a7e62440941e6
Devicharan Dasari <144323816+Charan-Dasari@users.noreply.github.com>
2026-07-28 16:52:40 +0530

    Delete backend directory

--- backend/Smart_Queue/Services/IPoiService.cs
+++
@@ -1,8 +0,0 @@
-using Smart_Queue.Models;
-
-namespace Smart_Queue.Services;
-
-public interface IPoiService
-{
-    Task<List<Smart_Queue.Models.ServiceProvider>> GetNearbyPoisAsync(double lat, double lon, double radiusMeters = 5000);
-}
